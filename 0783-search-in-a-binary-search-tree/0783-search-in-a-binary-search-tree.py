2845906860582217eac244a7a540699accf676cc
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2024-12-13 15:45:41 +0300

    Time: 0 ms (100%), Space: 19.3 MB (5.16%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.py
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.py
@@ -8,9 +8,12 @@ class Solution:
     def searchBST(self, root: TreeNode, val: int) -> TreeNode:
         if not root:
             return None
-        if root.val == val:
-            return root
-        if val < root.val:
-            return self.searchBST(root.left, val)
-        elif val > root.val:
-            return self.searchBST(root.right, val)
+
+        while root:
+            if val == root.val:
+                return root
+            elif val < root.val:
+                root = root.left
+            else:
+                root = root.right
+        return None
