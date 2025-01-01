f1a3f28ce8ff9f43d2207af9a3390b44413e226a
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2025-01-01 21:13:35 +0300

    Time: 288 ms (23.47%), Space: 27.8 MB (53.74%) - LeetHub

--- 0648-replace-words/0648-replace-words.py
+++ 0648-replace-words/0648-replace-words.py
@@ -1,51 +1,21 @@
-class Trie:
-    def __init__(self):
-        self.child = [None] * 26
-        self.is_end = False
-
-    def letter_index(self, letter):
-        return ord(letter) - ord('a')
-
-    def insert(self, str):
-        curr = self
-
-        for letter in str:
-            letter_index = self.letter_index(letter)
-            if curr.child[letter_index] is None:
-                curr.child[letter_index] = Trie()
-            curr = curr.child[letter_index]
-
-        curr.is_end = True
-
-    def prefix(self, prefix):
-        if self.child[self.letter_index(prefix[0])] is None:
-            return False, ''
-        curr = self
-        word = ''
-
-        for l in prefix:
-            l_idx = self.letter_index(l)
-            if curr.child[l_idx] is None:
-                break
-            word += chr(l_idx + ord('a'))
-            if curr.child[l_idx].is_end:
-                return True, word
-            curr = curr.child[l_idx]
-
-        return (True, word) if curr.is_end else (False, '')
-
-
 class Solution:
     def replaceWords(self, dictionary: list[str], sentence: str) -> str:
+        def prefix(d, word):
+            mystr = ''
+            for l in word:
+                mystr += l
+                if d.get(mystr) is not None:
+                    return mystr
+            return mystr
+
+        d = {}
+
+        for word in dictionary:
+            d[word] = True
+
         mylist = sentence.split()
-        trie = Trie()
 
-        for root in dictionary:
-            trie.insert(root)
-
-        for idx in range(len(mylist)):
-            flag, word = trie.prefix(mylist[idx])
-            if flag:
-                mylist[idx] = word
+        for i in range(len(mylist)):
+            mylist[i] = prefix(d, mylist[i])
 
         return ' '.join(mylist)
