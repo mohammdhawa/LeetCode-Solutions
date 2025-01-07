545cea43f06fdb664bb27c53be1fdf00ef1af62c
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2025-01-07 11:55:41 +0300

    Time: 117 ms (46.59%), Space: 21.3 MB (8.47%) - LeetHub

--- 0676-implement-magic-dictionary/0676-implement-magic-dictionary.py
+++ 0676-implement-magic-dictionary/0676-implement-magic-dictionary.py
@@ -1,40 +1,26 @@
-class Trie:
-    def __init__(self):
-        self.child = {}
-        self.is_end = False
-
-
 class MagicDictionary:
 
     def __init__(self):
-        self.root = Trie()
-
-    def _insert(self, word):
-        curr = self.root
-        for l in word:
-            if l not in curr.child:
-                curr.child[l] = Trie()
-            curr = curr.child[l]
-        curr.is_end = True
+        from collections import defaultdict
+        self.patterns_dict = defaultdict(list)
 
     def buildDict(self, dictionary: list[str]) -> None:
         for word in dictionary:
-            self._insert(word)
+            for i in range(len(word)):
+                word_pattern = word[:i] + '*' + word[i+1:]
+                self.patterns_dict[word_pattern].append(word)
 
-    def _search(self, word, index, count, trie):
-        if index == len(word):
-            return count == 1 and trie.is_end
-
-        char = word[index]
-        for key in trie.child:
-            if key == char:
-                if self._search(word, index + 1, count, trie.child[key]):
-                    return True
-            else:
-                if self._search(word, index + 1, count + 1, trie.child[key]):
-                    return True
+    def search(self, searchWord: str) -> bool:
+        for i in range(len(searchWord)):
+            word_pattern = searchWord[:i] + '*' + searchWord[i+1:]
+            if word_pattern in self.patterns_dict:
+                for word in self.patterns_dict[word_pattern]:
+                    if word != searchWord:
+                        return True
         return False
 
 
-    def search(self, searchWord: str) -> bool:
-        return self._search(searchWord, 0, 0, self.root)
+# Your MagicDictionary object will be instantiated and called as such:
+# obj = MagicDictionary()
+# obj.buildDict(dictionary)
+# param_2 = obj.search(searchWord)
