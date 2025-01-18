9c7bedee0c74a1d8b935db4919518495567ae7f3
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2025-01-18 17:01:34 +0300

    Time: 511 ms (43.72%), Space: 17.6 MB (58.47%) - LeetHub

--- 3507-find-the-count-of-numbers-which-are-not-special/3507-find-the-count-of-numbers-which-are-not-special.py
+++ 3507-find-the-count-of-numbers-which-are-not-special/3507-find-the-count-of-numbers-which-are-not-special.py
@@ -14,15 +14,14 @@ class Solution:
                     return False
             return True
 
+        count = 0
         myst = set()
         low, high = math.isqrt(l), math.isqrt(r) + 1
 
         for i in range(low, high + 1):
             if is_prime(i):
                 square = i ** 2
-                if square > r:
-                    break
-                myst.add(square)
+                if l <= square <= r:
+                    count += 1
 
-        count = sum(1 for i in myst if l <= i <= r)
         return (r - l + 1) - count
