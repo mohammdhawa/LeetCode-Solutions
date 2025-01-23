48c029121c30be8b7890ca9a699643fe091c1655
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2025-01-23 13:38:30 +0300

    Time: 3 ms (29.45%), Space: 17.9 MB (19.37%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,31 @@
+class Solution:
+    def __init__(self):
+        self.trie = {}
+
+    def _insert(self, word):
+        curr = self.trie
+        for letter in word:
+            if letter not in curr:
+                curr[letter] = {}
+            curr = curr[letter]
+
+
+    def longestCommonPrefix(self, strs):
+        _min = len(strs[0])
+        wrd = strs[0]
+        for word in strs:
+            if len(word) < _min:
+                _min = len(word)
+                wrd = word
+            self._insert(word)
+
+        curr = self.trie
+        result = ""
+        for letter in wrd:
+            if letter not in curr:
+                return result
+            if len(curr) > 1:
+                return result
+            result += letter
+            curr = curr[letter]
+        return result
