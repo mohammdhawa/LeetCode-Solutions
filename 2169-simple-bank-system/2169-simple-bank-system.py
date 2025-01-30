58703e1f5b4cde1cf72cea7808c83479bbbd5cbb
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2025-01-30 10:48:44 +0300

    Time: 63 ms (11.77%), Space: 61.1 MB (10.1%) - LeetHub

--- 2169-simple-bank-system/2169-simple-bank-system.py
+++ 2169-simple-bank-system/2169-simple-bank-system.py
@@ -1,33 +1,34 @@
 class Bank:
 
     def __init__(self, balance):
-        self.accounts = {}
-        for idx, val in enumerate(balance):
-            self.accounts[idx + 1] = val
+        self.accounts = {idx+1: val for idx, val in enumerate(balance)}
+
+    def _validate_account(self, account):
+        return self.accounts.get(account) is not None
+
+    def _validate_balance(self, account, balance):
+        return self._validate_account(account) and self.accounts[account] >= balance
 
     def transfer(self, account1: int, account2: int, money: int) -> bool:
-        if account1 not in self.accounts or account2 not in self.accounts:
-            return False
-        if self.accounts[account1] < money:
-            return False
-        self.accounts[account2] += money
-        self.accounts[account1] -= money
-        return True
+        if self._validate_balance(account1, money) and self._validate_account(account2):
+            if account1 == account2:
+                return True
+            self.accounts[account2] += money
+            self.accounts[account1] -= money
+            return True
+        return False
 
     def deposit(self, account: int, money: int) -> bool:
-        if account not in self.accounts:
+        if not self._validate_account(account):
             return False
         self.accounts[account] += money
         return True
 
     def withdraw(self, account: int, money: int) -> bool:
-        if account not in self.accounts:
-            return False
-        if self.accounts[account] < money:
+        if not self._validate_balance(account, money):
             return False
         self.accounts[account] -= money
         return True
-        
 
 
 # Your Bank object will be instantiated and called as such:
