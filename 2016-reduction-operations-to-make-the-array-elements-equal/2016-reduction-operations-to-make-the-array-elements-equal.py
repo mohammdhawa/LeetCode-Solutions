ea4d801c81418af9f48cf1384ca03758aa211c30
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2025-04-07 10:26:44 +0300

    Time: 118 ms (40.9%), Space: 23.3 MB (97.76%) - LeetHub

--- 2016-reduction-operations-to-make-the-array-elements-equal/2016-reduction-operations-to-make-the-array-elements-equal.py
+++ 2016-reduction-operations-to-make-the-array-elements-equal/2016-reduction-operations-to-make-the-array-elements-equal.py
@@ -7,17 +7,11 @@ class Solution:
 
         j = n - 1
         operations = 0
-        min_ele = nums[0]
 
-        # while j > 0:
-        #     if nums[j] == min_ele:
-        #         return operations
-        #     if nums[j] > nums[j - 1]:
-        #         operations += n - j
-        #     j -= 1
-        prev = 0
-        for i in range(1, n):
-            if nums[i] != nums[i - 1]:
-                prev += 1
-            operations += prev
+        while j > 0:
+            if nums[j] == nums[0]:
+                return operations
+            if nums[j] > nums[j - 1]:
+                operations += n - j
+            j -= 1
         return operations
