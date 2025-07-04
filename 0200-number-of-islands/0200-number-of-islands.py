eb01d2c12e6a0f9c74f161ceb41c52efc8eca91d
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2025-07-04 09:58:28 +0300

    Time: 247 ms (57.6%), Space: 20.3 MB (42.98%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,23 +4,24 @@ class Solution:
             return 0
 
         def vp(i, j, n, m):
-            if 0 <= i < n and 0 <= j < m and grid[i][j] == "1":
+            if 0 <= i < n and 0 <= j < m and grid[i][j] == '1':
                 return True
             return False
 
         def dfs(i, j, n, m):
-            if grid[i][j] == "v": return
-            grid[i][j] = "v"
+            if visited[i][j]: return
+            visited[i][j] = True
 
             for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                 if vp(dx + i, dy + j, n, m):
                     dfs(dx + i, dy + j, n, m)
 
         n, m =  len(grid), len(grid[0])
+        visited = [[False for _ in range(m)] for _ in range(n)]
         count = 0
         for i in range(n):
             for j in range(m):
-                if grid[i][j] == "1":
+                if grid[i][j] == "1" and not visited[i][j]:
                     dfs(i, j, n, m)
                     count += 1
         return count
