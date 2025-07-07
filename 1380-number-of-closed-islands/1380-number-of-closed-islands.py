46a05bca406747f0ab15ee3ba2ede24d58f1fa8a
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2025-07-07 10:29:39 +0300

    Time: 19 ms (56.93%), Space: 18.7 MB (34.23%) - LeetHub

--- 1380-number-of-closed-islands/1380-number-of-closed-islands.py
+++ 1380-number-of-closed-islands/1380-number-of-closed-islands.py
@@ -3,16 +3,13 @@ class Solution:
         n, m = len(grid), len(grid[0])
         visited = [[False for _ in range(m)] for _ in range(n)]
 
-        def vp(i, j):
-            return 0 <= i < n and 0 <= j < m
-
         def dfs(i, j):
             visited[i][j] = True
             flag = True
 
             for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
                 n_i, n_j = dx + i, dy + j
-                if not vp(n_i, n_j):
+                if not (0 <= n_i < n and 0 <= n_j < m):
                     flag = False
                 elif grid[n_i][n_j] == 0 and not visited[n_i][n_j]:
                     flag = dfs(n_i, n_j) and flag
