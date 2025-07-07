515d7039134552276855b4e329caca7687189a76
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2025-07-07 10:28:10 +0300

    Time: 23 ms (39.46%), Space: 18.8 MB (32.04%) - LeetHub

---
+++ 1380-number-of-closed-islands/1380-number-of-closed-islands.py
@@ -0,0 +1,29 @@
+class Solution:
+    def closedIsland(self, grid: list[list[int]]) -> int:
+        n, m = len(grid), len(grid[0])
+        visited = [[False for _ in range(m)] for _ in range(n)]
+
+        def vp(i, j):
+            return 0 <= i < n and 0 <= j < m
+
+        def dfs(i, j):
+            visited[i][j] = True
+            flag = True
+
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                n_i, n_j = dx + i, dy + j
+                if not vp(n_i, n_j):
+                    flag = False
+                elif grid[n_i][n_j] == 0 and not visited[n_i][n_j]:
+                    flag = dfs(n_i, n_j) and flag
+
+            return flag
+
+        count = 0
+        for i in range(n):
+            for j in range(m):
+                if not visited[i][j] and grid[i][j] == 0:
+                    if dfs(i, j):
+                        count += 1
+
+        return count
