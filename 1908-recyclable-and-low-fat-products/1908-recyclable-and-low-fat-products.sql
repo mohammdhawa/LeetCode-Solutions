a24705f5e9b657f84eed700d7064909d7128ef24
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2025-07-20 18:17:39 +0300

    Time: 605 ms (17.26%), Space: 0B (100%) - LeetHub

--- 1908-recyclable-and-low-fat-products/1908-recyclable-and-low-fat-products.sql
+++ 1908-recyclable-and-low-fat-products/1908-recyclable-and-low-fat-products.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-SELECT product_id FROM Products WHERE low_fats = 'Y' AND recyclable = 'Y';
+SELECT product_id FROM Products WHERE product_id NOT IN (SELECT product_id FROM Products WHERE low_fats = 'N' OR recyclable = 'N');
