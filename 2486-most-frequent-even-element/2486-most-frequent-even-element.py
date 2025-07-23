798ce5f279203e8352452a30d0060029a76afb23
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2025-07-23 17:44:42 +0300

    Time: 10 ms (98.01%), Space: 18 MB (95.38%) - LeetHub

--- 2486-most-frequent-even-element/2486-most-frequent-even-element.py
+++ 2486-most-frequent-even-element/2486-most-frequent-even-element.py
@@ -2,7 +2,13 @@ class Solution:
     def mostFrequentEven(self, nums: list[int]) -> int:
         from collections import Counter
 
-        d_counter = Counter([num for num in nums if num % 2 == 0])
+        d_counter = {}
+        for num in nums:
+            if num % 2 == 0:
+                if num in d_counter:
+                    d_counter[num] += 1
+                else:
+                    d_counter[num] = 1
         count = -1
         _key = -1
         for key, val in d_counter.items():
