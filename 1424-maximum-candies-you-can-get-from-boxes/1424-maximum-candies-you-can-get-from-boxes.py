222073d12e4bdf30093a7b3311530a10388a3bb7
Muhammed Recep Hava <66217824+mohammdhawa@users.noreply.github.com>
2025-08-07 14:40:24 +0300

    Time: 50 ms (15.34%), Space: 27.9 MB (98.91%) - LeetHub

--- 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.py
+++ 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.py
@@ -2,8 +2,8 @@ class Solution:
     def maxCandies(self, status: list[int], candies: list[int], keys: list[list[int]], containedBoxes: list[list[int]], initialBoxes: list[int]) -> int:
         from collections import deque
 
-        visited = set()
-        k = set()
+        visited = [False] * len(status)
+        k = [False] * len(status)
         queue = deque(initialBoxes)
         queue2 = deque()
         result = 0
@@ -11,14 +11,14 @@ class Solution:
         while queue:
             state = queue.popleft()
 
-            if state in visited:
+            if visited[state]:
                 continue
 
-            if status[state] or state in k:
-                visited.add(state)
+            if status[state] or k[state]:
+                visited[state] = True
                 result += candies[state]
-                for x in keys[state]:
-                    k.add(x)
+                for idx in keys[state]:
+                    k[idx] = True
                 for ele in containedBoxes[state]:
                     queue.append(ele)
                 while queue2:
